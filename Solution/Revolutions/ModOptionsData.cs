8d9ef2420d34dfae8832db369feead333de53392
zkajo <zkajota@gmail.com>
2020-04-27 09:46:14 +0100

    Changes from main branch

--- Solution/Revolutions/ModOptionsData.cs
+++ Solution/Revolutions/ModOptionsData.cs
@@ -9,5 +9,7 @@ namespace Revolutions
         [SaveableField(2)] public bool OverextensionMechanics = true;
         [SaveableField(3)] public bool PlayerAffectedByOverextension = true;
         [SaveableField(4)] public int DaysUntilLoyaltyChange = 80;
+        [SaveableField(5)] public bool AllowMinorFactions = false;
+        [SaveableField(6)] public bool DebugMode = false;
     }
 }
