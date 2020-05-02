4f598777112a9d785b50f005ec5f11ec295e93e7
zkajo <zkajota@gmail.com>
2020-05-02 18:37:56 +0100

    Updated cleanup to refresh after X ticks

--- Revolutions/CampaignBehaviors/CleanupBehaviour.cs
+++ Revolutions/CampaignBehaviors/CleanupBehaviour.cs
@@ -7,6 +7,9 @@ namespace Revolutions.CampaignBehaviors
 {
     public class CleanupBehaviour : CampaignBehaviorBase
     {
+        private int _currentTick = 0;
+        private const int _refreshAtTick = 100;
+        
         public override void RegisterEvents()
         {
             CampaignEvents.TickEvent.AddNonSerializedListener(this, new Action<float>(this.TickEvent));
@@ -14,6 +17,7 @@ namespace Revolutions.CampaignBehaviors
             CampaignEvents.OnPartyRemovedEvent.AddNonSerializedListener(this, new Action<PartyBase>(this.PartyRemovedEvent));
             CampaignEvents.OnLordPartySpawnedEvent.AddNonSerializedListener(this, new Action<MobileParty>(this.LordPartySpawned));
             CampaignEvents.OnClanDestroyedEvent.AddNonSerializedListener(this, new Action<Clan>(this.ClanDestroyedEvent));
+            CampaignEvents.PartyVisibilityChangedEvent.AddNonSerializedListener(this, new Action<PartyBase>(this.PartyVisibilityChangedEvent));
         }
 
         public override void SyncData(IDataStore dataStore)
@@ -29,6 +33,11 @@ namespace Revolutions.CampaignBehaviors
         private void PartyRemovedEvent(PartyBase party)
         {
             RevolutionsManagers.PartyManager.RemovePartyInfo(party.Id);
+        }
+
+        private void PartyVisibilityChangedEvent(PartyBase party)
+        {
+            
         }
 
         private void LordPartySpawned(MobileParty party)
@@ -50,10 +59,29 @@ namespace Revolutions.CampaignBehaviors
         {
             //TODO: Add similar functions for other manager
             //TODO: Better functionality here. These are too slow.
-            //RevolutionsManagers.FactionManager.WatchFactions();
-            //RevolutionsManagers.SettlementManager.WatchSettlements();
-            //RevolutionsManagers.PartyManager.WatchParties();
-            //RevolutionsManagers.ClanManager.WatchClans();
+
+            if (_currentTick == _refreshAtTick)
+            {
+                RevolutionsManagers.FactionManager.WatchFactions();
+            }
+            
+            if (_currentTick == _refreshAtTick + 10)
+            {
+                RevolutionsManagers.SettlementManager.WatchSettlements();
+            }
+            
+            if (_currentTick == _refreshAtTick + 20)
+            {
+                RevolutionsManagers.PartyManager.WatchParties();
+            }
+            
+            if (_currentTick > _refreshAtTick + 30)
+            {
+                RevolutionsManagers.ClanManager.WatchClans();
+                _currentTick = 0;
+            }
+            
+            _currentTick++;
         }
     }
 }
