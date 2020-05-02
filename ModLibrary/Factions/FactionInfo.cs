cb182046945db02bb944b1d52fe811895a1c7914
zkajo <zkajota@gmail.com>
2020-05-02 20:24:11 +0100

    Set initial towns count

--- ModLibrary/Factions/FactionInfo.cs
+++ ModLibrary/Factions/FactionInfo.cs
@@ -15,13 +15,14 @@ namespace ModLibrary.Factions
         public FactionInfo(IFaction faction)
         {
             this.FactionId = faction.StringId;
+            InitialTownsCount = ModLibraryManagers.FactionManager.GetFaction(this.FactionId).Settlements.Where(settlement => settlement.IsTown).Count();
         }
 
         public string FactionId { get; set; }
 
         public IFaction Faction => ModLibraryManagers.FactionManager.GetFaction(this.FactionId);
 
-        public int InitialTownsCount { get; set; } = 0;
+        public int InitialTownsCount { get; set; }
 
         public int CurrentTownsCount => ModLibraryManagers.FactionManager.GetFaction(this.FactionId).Settlements.Where(settlement => settlement.IsTown).Count();
 
