ad6cd2f303f68f2a1ee9baed13230c0d97816451
TH3UNKN0WN <TH3UNKN0WN@crabgrinder.com>
2020-05-08 13:36:59 +0200

    [0.4.7] Added filtering for updating parties.

---
+++ ModLibrary/Patches/ObjectTypeRecordPatch.cs
@@ -0,0 +1,17 @@
+﻿using HarmonyLib;
+using TaleWorlds.ObjectSystem;
+using static TaleWorlds.ObjectSystem.ObsoleteObjectManager;
+
+namespace ModLibrary.Patches
+{
+    [HarmonyPatch(typeof(ObjectTypeRecord<MBObjectBase>))]
+    public class ObjectTypeRecordPatch
+    {
+        [HarmonyPostfix]
+        [HarmonyPatch("RegisterObject")]
+        public static void PostfixRegisterObject(MBObjectBase obj, bool presumed, MBObjectBase registeredObject)
+        {
+
+        }
+    }
+}
---
+++ ModLibrary/Patches/Patcher.cs
@@ -0,0 +1,12 @@
+﻿using HarmonyLib;
+
+namespace ModLibrary.Patches
+{
+    public static class Patcher
+    {
+        public static void Patch()
+        {
+            new Harmony("com.kntmods.bannerlord.revolutions").PatchAll();
+        }
+    }
+}
