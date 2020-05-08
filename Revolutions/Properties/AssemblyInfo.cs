ea89d2bfbadaf6d1065e23bf71d7330e46cb3cea
TH3UNKN0WN <TH3UNKN0WN@crabgrinder.com>
2020-05-08 23:08:17 +0200

    [0.4.9] Version Bump.

--- Revolutions/Properties/AssemblyInfo.cs
+++ Revolutions/Properties/AssemblyInfo.cs
@@ -31,6 +31,6 @@ using System.Runtime.InteropServices;
 // You can specify all the values or you can default the Build and Revision Numbers
 // by using the '*' as shown below:
 // [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("0.4.8")]
-[assembly: AssemblyFileVersion("0.4.8")]
-[assembly: AssemblyInformationalVersion("Mount & Blade II: Bannerlord | BETA 1.3.0")]
+[assembly: AssemblyVersion("0.4.9")]
+[assembly: AssemblyFileVersion("0.4.9")]
+[assembly: AssemblyInformationalVersion("Mount & Blade II: Bannerlord | 1.3.1 & 1.4.0")]
